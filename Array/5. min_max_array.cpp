a7d211774724b5ed6d5bddafb7ee9d2cbac993eb
Asfak Husain <asfakhusain99@gmail.com>
2022-05-31 11:59:35 +0530

    working with arrays

--- Array/5. min_max_array.cpp
+++ Array/5. min_max_array.cpp
@@ -1,15 +1,59 @@
+// ? Linear Method for finding min max using structure
+
 #include<bits/stdc++.h>
 using namespace std;
 
+struct  MIN_MAX
+{
+     int min;
+     int max;
+};
 
-int min_array(int *arr, int sizeOfArray){
-     for(int i=0; i<sizeOfArray; i++){
-                               
+
+MIN_MAX get_min_max(int *arr, int sizeOfArray){
+     struct MIN_MAX min_max;
+     if (sizeOfArray == 1){
+          min_max.min = arr[0];
+          min_max.max = arr[0];
+          return min_max;
      }
+     // check for two elements
+     if(arr[0] > arr[1]){
+          min_max.min = arr[0];
+          min_max.max = arr[1];
+     }else{
+          min_max.min = arr[1];
+          min_max.max = arr[0];
+
+     }
+
+     // check for 3 and more elements
+
+     for(int i = 2; i< sizeOfArray; i++){
+          if(arr[i]>min_max.max){
+               min_max.max = arr[i];
+          }
+          else if(arr[i] < min_max.min){
+               min_max.min = arr[i];
+          }
+     }
+
+         
+     return min_max;
 }
 
 int main(){
      int arr[] = {-2,3,4,-1,5,-12,6,1,3};
      int sizeOfArray = sizeof(arr)/sizeof(int);
-     int min = min_array(arr,sizeOfArray);
+     struct MIN_MAX  min_max = get_min_max(arr,sizeOfArray);
+
+     cout << "min" << min_max.min << endl;
+     cout << "max" << min_max.max;
 }
+
+
+// time complexity --  O(n)
+// space complexity  -- O(1)
+
+// in best case (ascending order of element) (n-2 ) + 1 comparisions
+//  in worst case (descending order of element) 2(n-2) +1 comparisions
---
+++ Array/6. min_max_tournament_method.cpp
@@ -0,0 +1,72 @@
+// ? Tournament or knockout Method for finding min max using structure
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct  MIN_MAX
+{
+     int min;
+     int max;
+};
+
+
+MIN_MAX get_min_max(int *arr, int low,int high){
+     
+
+     struct MIN_MAX min_max;
+     
+     // for single element array
+     if(low == high) {
+          min_max.min = arr[high];
+          min_max.max = arr[high];
+          return min_max;
+     }
+
+     // for two elements in the array
+     if(high == low+1){
+          if(arr[low] < arr[high]){
+               min_max.min = arr[low];
+               min_max.max = arr[high];
+          }else{
+               min_max.min = arr[high];
+               min_max.max = arr[low];
+          }
+     }
+
+     // for more than 2 elements in the array
+     int mid = (low+ high)/2;
+     struct MIN_MAX min_max_left = get_min_max(arr, low, mid);
+     struct MIN_MAX min_max_right = get_min_max(arr, mid+1, high);
+
+     if(min_max_left.min < min_max_right.min ){
+          min_max.min = min_max_left.min;
+     }else{
+          min_max.min = min_max_right.min;
+     }
+
+     if(min_max_left.max < min_max_right.max){
+          min_max.max = min_max_right.max;
+     }else{
+          min_max.max = min_max_left.max;
+     }
+
+
+     return min_max;
+
+}
+
+int main(){
+     int arr[] = {-2,3,4,-1,5,-12,6,1,3};
+     int sizeOfArray = sizeof(arr)/sizeof(int);
+     struct MIN_MAX  min_max = get_min_max(arr,0,sizeOfArray);
+
+     cout << "min" << min_max.min << endl;
+     cout << "max" << min_max.max;
+}
+
+
+// time complexity --  O(n)
+// space complexity  -- O(1)
+
+// in best case (ascending order of element) (n-2 ) + 1 comparisions
+//  in worst case (descending order of element) 2(n-2) +1 comparisions
